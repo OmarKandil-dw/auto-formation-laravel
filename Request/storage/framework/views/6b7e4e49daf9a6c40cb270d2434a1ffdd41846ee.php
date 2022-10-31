e0b9254667995d68715a707cc930d2bb60480811
omar_kandil <93253683+OmarKandil-dw@users.noreply.github.com>
2022-10-31 01:54:09 -0700

    Delete Request directory

--- Request/storage/framework/views/6b7e4e49daf9a6c40cb270d2434a1ffdd41846ee.php
+++
@@ -1,7 +0,0 @@
-<?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-<div>
-    id : <?php echo e($item->id); ?> , nom : <?php echo e($item->nom); ?>, <a href="">Delete</a>
-</div>
-    
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-<?php /**PATH C:\Users\Kandil\brief2\Controlle\resources\views/select.blade.php ENDPATH**/ ?>
--- Request/storage/framework/views/dc6b51874b654810e9b7a82bc709509c1e0c172b.php
+++
@@ -1,4 +0,0 @@
-<form action="/insert" method="get">
-    <input type="text" name="name">
-    <button>Click</button>
-</form><?php /**PATH C:\Users\Kandil\brief2\Controlle\resources\views/add.blade.php ENDPATH**/ ?>
