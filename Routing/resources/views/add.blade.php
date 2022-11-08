d76751ae10a8eb4fed90eceb505a738cb2c6ee8b
omar_kandil <93253683+OmarKandil-dw@users.noreply.github.com>
2022-11-08 02:10:13 -0800

    Delete Routing directory

--- Routing/resources/views/add.blade.php
+++
@@ -1,4 +0,0 @@
-{{-- <form action="/insert" method="get">
-    <input type="text" name="nom">
-    <button>Click</button>
-</form> --}}
--- Routing/resources/views/select.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- @foreach ($data as $item)
-<div>
-    id : {{ $item->id }} , nom : {{ $item->nom }}, <a href="">Delete</a>
-</div>
-    
-@endforeach --}}
